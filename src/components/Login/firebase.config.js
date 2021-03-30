e41bc895712175b0d870aaee299dd2e8b110499a
Mahabub Sunny <office.prosany@gmail.com>
2021-03-30 20:03:06 +0600

    Load Data from Database

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjf_eK14LDQHt1d7_lSBIKMuOj52cVnpY",
+  authDomain: "burj-al-arab-c5535.firebaseapp.com",
+  projectId: "burj-al-arab-c5535",
+  storageBucket: "burj-al-arab-c5535.appspot.com",
+  messagingSenderId: "633058104315",
+  appId: "1:633058104315:web:a6dfd5452a5e918c5e315e"
+};
+export default firebaseConfig;
